3c1db5145c894115ec4528829d1601250038c212
Vitaly Mosoriuk <vitmsrk@gmail.com>
2017-02-10 16:02:56 +0200

    State-menu [40%]

--- app/components/canvasBox/canvasBox.directive.js
+++ app/components/canvasBox/canvasBox.directive.js
@@ -13,13 +13,43 @@
 		}
 	}
 
-	function canvasBoxCtrl($scope, $compile, $document) {
+	function canvasBoxCtrl($scope, $compile, $mdMenu) {
 		const src = 'http://www.w3.org/2000/svg';
 		const xlink = 'http://www.w3.org/1999/xlink';
+
+		var menuCtrl = {
+			open: function (event) {
+				$scope.activeState = $scope.tabs[$scope.current].states[$scope.tabs[$scope.current].states.map(function (s) { return s.id }).indexOf(event.target.getAttribute('id'))];
+				$mdMenu.show({
+					scope: $scope,
+					mdMenuCtrl: menuCtrl,
+					element: angular.element(angular.copy(document.getElementById('state-menu').firstElementChild)),
+					target: event.target
+				});
+			},
+			close: function () {
+				$scope.activeState = null;
+				$mdMenu.hide();
+			},
+			positionMode: function () {
+				return {
+					left: 'target',
+					top: 'target'
+				};
+			},
+			offsets: function () {
+				return {
+					top: $scope.preferences.stateRadius * 1.5 * $scope.tabs[$scope.current].scale,
+					left: $scope.preferences.stateRadius * 2 * $scope.tabs[$scope.current].scale
+				};
+			}
+		};
 
 		$scope.tabsCount = 1;
 		$scope.tabs = [new Tab($scope.tabsCount)];
 		$scope.current = 0;
+		$scope.hasDragged = false;
+		$scope.activeState = null;
 
 		$scope.addTab = function () {
 			$scope.tabs.push(new Tab(++$scope.tabsCount));
@@ -31,6 +61,7 @@
 
 		$scope.drawState = function (event, tab) {
 			var state = new State(tab.index, ++tab.statesCount, $scope.preferences.stateNamePrefix + (tab.statesCount - 1));
+			tab.states.push(state);
 
 			var circle = document.createElementNS(src, 'circle');
 			circle.setAttributes({
@@ -58,16 +89,32 @@
 			var use = document.createElementNS(src, 'use');
 			use.setAttributeNS(xlink, 'href', '#' + g.getAttribute('id'));
 			use.setAttributes({
+				'id': state.id,
 				'x': event.offsetX / $scope.tabs[$scope.current].scale,
 				'y': event.offsetY / $scope.tabs[$scope.current].scale,
 				'class': 'state',
-				'ng-attr-transform': 'scale({{tabs[current].scale}})'
+				'ng-attr-transform': 'scale({{tabs[current].scale}})',
+				'ng-click': 'openMenu($event)'
 			});
 			use.draggable();
 
 			var canvas = document.getElementById(tab.id);
 			canvas.appendChild($compile(defs)($scope)[0]);
 			canvas.appendChild($compile(use)($scope)[0]);
+		};
+
+		$scope.removeState = function () {
+			alert();
+			console.log($scope.activeState);
+			$scope.tabs[$scope.current].states.splice($scope.tabs[$scope.current].states.indexOf($scope.activeState), 1);
+		};
+
+		$scope.a = function () {
+			console.log('success');
+		};
+
+		$scope.openMenu = function (event) {
+			$scope.hasDragged ? $scope.hasDragged = false : menuCtrl.open(event);
 		};
 
 		function Tab(index) {
@@ -82,7 +129,6 @@
 			this.id = 'state-' + tabIndex + index;
 			this.index = index;
 			this.name = name;
-			this.svg = null;
 		}
 
 		Element.prototype.setAttributes = function (attrs) {
@@ -101,6 +147,7 @@
 			});
 
 			function mouseMove(event) {
+				$scope.hasDragged = true;
 				that.setAttribute('x', that.x.animVal.value + event.movementX / $scope.tabs[$scope.current].scale);
 				that.setAttribute('y', that.y.animVal.value + event.movementY / $scope.tabs[$scope.current].scale);
 				for (var i in targets) {
